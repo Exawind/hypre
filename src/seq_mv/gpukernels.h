9c8dc46e0bb0b6fedabe767712e3fe13775c44cb
Michael Demoret <mdemoret@nvidia.com>
2018-06-07 15:36:54 -0600

    Adding a new parallel function to be used in par_relax.c

--- src/seq_mv/gpukernels.h
+++ src/seq_mv/gpukernels.h
@@ -5,5 +5,18 @@ void VecCopy(double* tgt, const double* src, int size,cudaStream_t s);
 void VecSet(double* tgt, int size, double value, cudaStream_t s);
 void VecScale(double *u, double *v, double *l1_norm, int num_rows,cudaStream_t s);
 void VecScaleSplit(double *u, double *v, double *l1_norm, int num_rows,cudaStream_t s);
+void ParRelax(
+      int n,
+      int relax_points,
+      int * cf_marker,
+      int * A_diag_i,
+      int * A_diag_j,
+      double * A_diag_data,
+      int * A_offd_i,
+      int * A_offd_j,
+      double * A_offd_data,
+      double * Vext_data,
+      double * f_data,
+      double * u_data);
 void CudaCompileFlagCheck();
 #endif
